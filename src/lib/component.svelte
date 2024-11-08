76db198fc6efc5af78d53bd96628e65475e72127
Boofdev <hexahigh0@gmail.com>
2024-11-08 13:03:30 +0100

    init

---
+++ src/lib/component.svelte
@@ -0,0 +1,1 @@
+<p>Hello, im a component!</p>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Component from "$lib/component.svelte";
+</script>
+
+<div>
+    <p>Hello, im the root page</p>
+    <Component />
+</div>
